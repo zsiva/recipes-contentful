6266039a9899d4fae4355bd5f04fe5ef62daf76f
Marta Carrizo <m.carrizo.ibarra@gmail.com>
2021-02-23 23:47:58 +0100

    Add provider to app

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,27 @@ import {
   LanguageContext,
   useLanguageContextValue,
 } from './Language/LanguageProvider';
+import RecipePage from './Dashboard/Recipes/RecipePage';
+import { RecipesContext } from './Dashboard/Recipes/RecipesProvider';
+import { useRecipesContextValue } from './Dashboard/Recipes/useRecipesContextValue';
 
 export default function App() {
   const languageContextValue = useLanguageContextValue();
+  const recipesContextValue = useRecipesContextValue();
   return (
     <LanguageContext.Provider value={languageContextValue}>
       <Router>
         <MenuBar />
-        <Switch>
-          <Route path='/'>
-            <Dashboard />
-          </Route>
-        </Switch>
+        <RecipesContext.Provider value={recipesContextValue}>
+          <Switch>
+            <Route path='/:slug'>
+              <RecipePage />
+            </Route>
+            <Route path='/'>
+              <Dashboard />
+            </Route>
+          </Switch>
+        </RecipesContext.Provider>
       </Router>
     </LanguageContext.Provider>
   );
--- src/Dashboard/Dashboard.tsx
+++ src/Dashboard/Dashboard.tsx
@@ -1,25 +1,20 @@
 import React from 'react';
 import Banner from './Banner';
-import { RecipesContext } from './Recipes/RecipesProvider';
 import RecipesList from './Recipes/RecipesList';
-import { useRecipesContextValue } from './Recipes/useRecipesContextValue';
 import FiltersContainer from './Filters/FiltersContainer';
 import { FiltersContext } from './Filters/FiltersProvider';
 import { useFiltersContextValue } from './Filters/useFiltersContextValue';
 
 const Dashboard = () => {
-  const recipesContextValue = useRecipesContextValue();
   const filtersDefaultValue = useFiltersContextValue();
   return (
-    <RecipesContext.Provider value={recipesContextValue}>
-      <main>
-        <Banner />
-        <FiltersContext.Provider value={filtersDefaultValue}>
-          <FiltersContainer />
-          <RecipesList />
-        </FiltersContext.Provider>
-      </main>
-    </RecipesContext.Provider>
+    <main>
+      <Banner />
+      <FiltersContext.Provider value={filtersDefaultValue}>
+        <FiltersContainer />
+        <RecipesList />
+      </FiltersContext.Provider>
+    </main>
   );
 };
 
