ff650894665f41fe75d1085b7de21ec595e53378
Marta <m.carrizo.ibarra@gmail.com>
2021-04-06 19:58:22 +0200

    use env variables

--- src/contentful/client.ts
+++ src/contentful/client.ts
@@ -1,6 +1,9 @@
 import { createClient } from 'contentful';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export const client = createClient({
-  accessToken: 'iZw1bJ-xzJSLcMWiWF3dRdaXkS0KDi9xJS9iZOiFE9w', //process.env.REACT_APP_ACCESS_TOKEN as string,
-  space: 'b1kxdns6lgrg' // process.env.REACT_APP_SPACE_ID as string,
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN as string,
+  space: process.env.CONTENTFUL_SPACE_ID as string,
 });
